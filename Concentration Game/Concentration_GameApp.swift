3b36e4075347df5ed3abd088ad312db991710916
Aiana <Aiana.Miachina@gmail.com>
2021-02-08 18:26:47 +0900

    Initial Commit

---
+++ Concentration Game/Concentration_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Concentration_GameApp.swift
+//  Concentration Game
+//
+//  Created by Aiana Miachina on 08.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct Concentration_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
